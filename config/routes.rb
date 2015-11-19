4e0ac5d306189240ebbc11fb3fbad0e28d8c0392
anhtd <ta.duy.anh@framgia.com>
2015-11-19 16:02:18 +0700

    Add layout, html for homepage, word and lession page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#home'
+  get 'words' => 'words#index', as: :words
+  get 'lessions' => 'categories#index', as: :lessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

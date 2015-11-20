88460fb20ab02bfd4ce1a853476ded32976b8978
anhtd <ta.duy.anh@framgia.com>
2015-11-20 10:58:13 +0700

    Finish static pages for frontend

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,13 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def start_lesson
+  end
+
+  def finish_lesson
+  end
+
+  def lesson_result
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def home
   end
+
+  def profile
+  end
+
+  def follow
+  end
 end
